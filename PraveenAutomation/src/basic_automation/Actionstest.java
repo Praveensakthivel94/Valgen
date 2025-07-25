edddb8b7d5ce1861ceebeefd16753baab92ade74
Praveensakthivel94 <84221686+Praveensakthivel94@users.noreply.github.com>
2025-07-25 19:25:36 +0530

    Update Actionstest.java
    PS -Action class updated

--- PraveenAutomation/src/basic_automation/Actionstest.java
+++ PraveenAutomation/src/basic_automation/Actionstest.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.interactions.Actions;
 
 public class Actionstest {
-	
+	//Testing Action Class
 	public static void main (String[] args) throws InterruptedException {
 		WebDriver driver=new EdgeDriver();
 		driver.get("https://www.flipkart.com/");
